c52e9105ce2f0df5c29ed5c7761255bf68d2e100
Esther Kim <ekim51@hawk.iit.edu>
2020-10-22 12:27:26 -0500

    week 9 assignment

--- NodeJS/Project/app.js
+++ NodeJS/Project/app.js
@@ -5,8 +5,10 @@ const app = express();
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
-// middleware
+const userRoutes = require('./api/routes/users');
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/users', userRoutes);
 
 module.exports = app;
