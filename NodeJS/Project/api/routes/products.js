7125b52a8d0d1a4917017592eedc7a16b6472add
Esther Kim <ekim51@hawk.iit.edu>
2020-11-23 13:58:04 -0600

    week 12 class

--- NodeJS/Project/api/routes/products.js
+++ NodeJS/Project/api/routes/products.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
+
+const Product = require('../models/product_model');
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
@@ -8,10 +11,21 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-    const product = {
+    // const product = {
+    //     name: req.body.name,
+    //     price: req.body.price
+    // };
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    };
+    });
+    product
+        .save()
+        .then(result => {
+            console.log(result)
+        })
+        .catch(err => console.log(err));
     res.status(201).json({ // 201 Created
         message: 'Handling POST requests to /products',
         createdProduct: product
@@ -20,17 +34,27 @@ router.post('/', (req, res, next) => {
 
 router.get('/:productID', (req, res, next) => {
     const id = req.params.productID;
-    if (id === 'special') {
-        res.status(200).json({
-            message: 'You discovered the special ID',
-            id: id
+    Product.findById(id)
+        .exec()
+        .then(doc => {
+            console.log("From DB:", doc);
+            res.status(200).json(doc);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({error: err});
         });
-    } else {
-        res.status(200).json({
-            message: 'You passed a normal ID',
-            id: id
-        });
-    };
+    // if (id === 'special') {
+    //     res.status(200).json({
+    //         message: 'You discovered the special ID',
+    //         id: id
+    //     });
+    // } else {
+    //     res.status(200).json({
+    //         message: 'You passed a normal ID',
+    //         id: id
+    //     });
+    // };
 });
 
 router.patch('/:productID', (req, res, next) => {
--- NodeJS/Project/app.js
+++ NodeJS/Project/app.js
@@ -4,14 +4,34 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 const userRoutes = require('./api/routes/users');
 
+mongoose.connect('mongodb+srv://ekim:' + process.env.MONGO_ATLAS_PW + 
+    '@cluster0.zrkhj.mongodb.net/test?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    });
+
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        if (req.method === 'OPTIONS') {
+            res.header('Access-Control-Allow-Methods',
+                'PUT, POST, PATCH, DELETE, GET');
+            return res.status(200).json({});
+        }
+        next();
+});
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
